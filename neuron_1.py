e488768de32a6fe2ce0a415f8437871c3bd0921c
Andrey Kropotin <tork713@gmail.com>
2023-04-27 07:30:31 +0700

    ввел функцию активации onestep(x)

--- neuron_1.py
+++ neuron_1.py
@@ -1,8 +1,14 @@
 # Модуль Neuron
 import numpy as np
 
-# Создание класса "Нейрон"
+def onestep(x):
+    b = 5
+    if x >= b:
+        return 1
+    else:
+        return 0
 
+# Создание класса "Нейрон"
 
 class Neuron:
     def __init__(self, w):
@@ -10,11 +16,11 @@ class Neuron:
 
     def y(self, x):             # Сумматор
         s = np.dot(self.w, x)     # Суммируем входы
-        return s                # функция активации
+        return onestep(s)                # функция активации
 
 
 Xi = np.array([1, 0, 0, 1])           # Задание значений входам
 Wi = np.array([5, 4, 3, 1])           # Веса входных сенсоров
 n = Neuron(Wi)                        # Создание объекта класса Neuron
-print('S1 = ', n.y(Xi))               # Обращение к нейрону
+print('Y = ', n.y(Xi))               # Обращение к нейрону
 
