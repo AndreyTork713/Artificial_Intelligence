8db16a9be91960c354af730649225abd04ab1f7c
Andrey Kropotin <tork713@gmail.com>
2023-05-03 08:18:37 +0700

    # Обучающая выборка (идеальное изображение цифр от 0 до 9)

--- neuron_1.py
+++ neuron_1.py
@@ -1,6 +1,7 @@
 # Модуль Neuron
 import numpy as np
 
+# функция активации
 def onestep(x):
     b = 5
     if x >= b:
@@ -19,7 +20,7 @@ class Neuron:
         return onestep(s)                # функция активации
 
 
-Xi = np.array([1, 0, 0, 1])           # Задание значений входам
+Xi = np.array([0, 0, 1, 1])           # Задание значений входам
 Wi = np.array([5, 4, 3, 1])           # Веса входных сенсоров
 n = Neuron(Wi)                        # Создание объекта класса Neuron
 print('Y = ', n.y(Xi))               # Обращение к нейрону
